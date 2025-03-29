662653ead2e7300e401e1304178c5cc0a3bdd222
Anara <berdali.aygerim9@gmail.com>
2025-03-29 14:08:51 +0400

    createAsyncThunk builder set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Navbar from './components/Navbar';
 import Modal from './components/Modal';
 import CartContainer from './components/CartContainer';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 function App() {
   const dispatch = useDispatch();
-  const {cartItems} = useSelector((store) => store.cart)
+  const {cartItems, isLoading} = useSelector((store) => store.cart)
   const {isOpen} = useSelector((store) => store.modal)
   
   //invoke useEffect every time there is a change in cartItems array
@@ -15,6 +15,18 @@ function App() {
     dispatch(calculateTotals())
 
   }, [cartItems])
+    //invoke useEffect only when app loads
+    useEffect(()=> {
+      dispatch(getCartItems())
+    }, [])
+
+    if(isLoading){
+      return (
+        <div className='loading'>
+          <h1>Loading ...</h1>
+        </div>
+      )
+    }
   return  (
     <main>
        <Navbar/>
--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,15 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
 
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
+
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 4,
   total:0,
   isLoading: true,
 
 }
 
-
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url).then((response) => response.json()).catch((error) => console.log(error)) 
+  })
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
@@ -42,6 +46,23 @@ const cartSlice = createSlice({
     state.amount = amount;
     state.total = total;
    }
+  },
+  // builder callback notation, gives access to both the state and action
+
+  extraReducers:(builder) => {
+    builder
+    .addCase(getCartItems.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(getCartItems.fulfilled, (state, action) => {
+      console.log(action)
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    })
+    .addCase(getCartItems.rejected, (state, action) => {
+      console.log(action)
+      state.isLoading = false;
+    });
   },
 })
 

1d5d9359651d8fe66ae28eb7372e60cb0cee56ca
Anara <berdali.aygerim9@gmail.com>
2025-03-29 12:02:35 +0400

    calculateTotal done, total number fixed to 2  to appear after the decimal point

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
-
+import { calculateTotals } from './features/cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  const {cartItems} = useSelector((store) => store.cart)
+  
+  //invoke useEffect every time there is a change in cartItems array
+  useEffect(()=> {
+    dispatch(calculateTotals())
+
+  }, [cartItems])
   return  (
     <main>
        <Navbar/>
